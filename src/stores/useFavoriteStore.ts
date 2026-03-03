2cf08cc883d644d926fdcb87308e19f5e455c498
Rubén Fernández Pérez <ruben@MacBook-Air-de-Ruben.local>
2026-03-03 15:13:21 +0100

    refactor(store): mover isFavoriteTeam de actions a getters

--- src/stores/useFavoriteStore.ts
+++ src/stores/useFavoriteStore.ts
@@ -4,15 +4,21 @@ export const useFavoriteStore = defineStore('favorite', {
   state: (): { favoriteTeamId: number | null } => ({
     favoriteTeamId: null,
   }),
+  getters: {
+    isFavoriteTeam: (state) => (teamId: number) => state.favoriteTeamId === teamId,
+    // Versión equivalente:
+    // isFavoriteTeam: (state) => {
+    //   return (teamId: number) => {
+    //     return state.favoriteTeamId === teamId
+    //   }
+    // },
+  },
   actions: {
     setFavoriteTeam(teamId: number) {
       this.favoriteTeamId = teamId
     },
     clearFavoriteTeam() {
       this.favoriteTeamId = null
-    },
-    isFavoriteTeam(teamId: number) {
-      return this.favoriteTeamId === teamId
     },
   },
 })
