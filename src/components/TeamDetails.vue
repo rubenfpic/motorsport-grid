9b8d82d425051bb1cf0eff81ac7ccc1575ab8541
Rubén Fernández Pérez <ruben@MacBook-Air-de-Ruben.local>
2026-03-03 15:16:20 +0100

    feat(team): integrar favorito en TeamDetails usando favorite store

--- src/components/TeamDetails.vue
+++ src/components/TeamDetails.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { useFavoriteStore } from '@/stores/useFavoriteStore'
 import type { Team } from '@/types'
+
+const favorite = useFavoriteStore()
 
 defineProps<{
   team: Team
@@ -7,7 +10,11 @@ defineProps<{
 </script>
 
 <template>
-  <h2>{{ team.name }}</h2>
+  <h2>
+    {{ team.name }}
+    <button v-if="favorite.isFavoriteTeam(team.id)" @click="favorite.clearFavoriteTeam()">☆</button>
+    <button v-else @click="favorite.setFavoriteTeam(team.id)">★</button>
+  </h2>
   <p><strong>ID:</strong> {{ team.id }}</p>
   <p><strong>País:</strong> {{ team.country }}</p>
   <p><strong>Fundado:</strong> {{ team.formed }}</p>
