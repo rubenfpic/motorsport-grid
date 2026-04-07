1c2163a736b5b30196445b9c9d0716cf978e305b
Rubén Fernández Pérez <ruben@MacBook-Air-de-Ruben.local>
2026-04-07 12:35:37 +0200

    refactor(event-overview): show event id using status-badge

--- src/components/events/EventOverview.vue
+++ src/components/events/EventOverview.vue
@@ -8,11 +8,11 @@ defineProps<{
 
 <template>
   <ul>
-    <li><strong>Event ID:</strong> {{ eventDetails.id }}</li>
     <li><strong>Season:</strong> {{ eventDetails.season }}</li>
     <li><strong>Venue:</strong> {{ eventDetails.venue }}</li>
     <li><strong>City:</strong> {{ eventDetails.city }}</li>
     <li><strong>Country:</strong> {{ eventDetails.country }}</li>
     <li v-if="eventDetails.date"><strong>Date:</strong> {{ eventDetails.date }}</li>
+    <li><status-badge :label="`id: ${eventDetails.id}`" /></li>
   </ul>
 </template>
